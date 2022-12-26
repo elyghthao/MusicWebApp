0cab398d97c18800aeb8cc3a03171c47e00fa65b
elyghthao <elyghthao@gmail.com>
2022-12-25 18:48:27 -0800

    changes

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -18,8 +18,8 @@ export const genres = [
 ];
 
 export const links = [
-  { name: 'Discover', to: '/', icon: HiOutlineHome },
-  { name: 'Around You', to: '/around-you', icon: HiOutlinePhotograph },
-  { name: 'Top Artists', to: '/top-artists', icon: HiOutlineUserGroup },
-  { name: 'Top Charts', to: '/top-charts', icon: HiOutlineHashtag },
+  { name: 'Discover', to: '/MusicWebApp', icon: HiOutlineHome },
+  { name: 'Around You', to: '/MusicWebApp/around-you', icon: HiOutlinePhotograph },
+  { name: 'Top Artists', to: '/MusicWebApp/top-artists', icon: HiOutlineUserGroup },
+  { name: 'Top Charts', to: '/MusicWebApp/top-charts', icon: HiOutlineHashtag },
 ];
