6269b9902f1ba6dd4f26725147d3a13de45b8cc8
elyghthao <elyghthao@gmail.com>
2022-12-25 18:51:13 -0800

    d

--- src/App.jsx
+++ src/App.jsx
@@ -17,12 +17,12 @@ const App = () => {
           <div className="flex-1 h-fit pb-40">
             <Routes>
               <Route path="/MusicWebApp/" element={<Discover />} />
-              <Route path="/top-artists" element={<TopArtists />} />
-              <Route path="/top-charts" element={<TopCharts />} />
-              <Route path="/around-you" element={<AroundYou />} />
-              <Route path="/artists/:id" element={<ArtistDetails />} />
-              <Route path="/songs/:songid" element={<SongDetails />} />
-              <Route path="/search/:searchTerm" element={<Search />} />
+              <Route path="/MusicWebApp/top-artists" element={<TopArtists />} />
+              <Route path="/MusicWebApp/top-charts" element={<TopCharts />} />
+              <Route path="/MusicWebApp/around-you" element={<AroundYou />} />
+              <Route path="/MusicWebApp/artists/:id" element={<ArtistDetails />} />
+              <Route path="/MusicWebApp/songs/:songid" element={<SongDetails />} />
+              <Route path="/MusicWebApp/search/:searchTerm" element={<Search />} />
             </Routes>
           </div>
 
