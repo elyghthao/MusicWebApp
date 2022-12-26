387c10ac5581b13b6342496f759505b91a46ccc2
elyghthao <elyghthao@gmail.com>
2022-12-25 19:35:45 -0800

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from 'react-redux';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes , HashRouter} from 'react-router-dom';
 
 import { Searchbar, Sidebar, MusicPlayer, TopPlay } from './components';
 import { ArtistDetails, TopArtists, AroundYou, Discover, Search, SongDetails, TopCharts } from './pages';
